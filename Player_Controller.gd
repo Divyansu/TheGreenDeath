dd31885531c1dff3e0f47615f72a81c91ef0ee6b
Divyansu Ambastha <rishuambasta@gmail.com>
2025-04-19 17:57:10 +0530

    Added wall jump
    *8 Directional Dashing missing
    *wall jump needs fine tuning

--- Player_Controller.gd
+++ Player_Controller.gd
@@ -5,6 +5,9 @@ extends CharacterBody2D
 @export_range(0,1) var deceleration = 0.1
 @export_range(0,1) var acceleration = 0.1
 
+@export var wall_slide_speed = 50.0
+@export var wall_jump_pushback = 200.0
+
 @export var jump_force = -400
 @export_range(0,1) var decelerate_on_jump_release = 0.5
 
@@ -25,36 +28,54 @@ var dash_timer = 0
 
 
 func _physics_process(delta):
-	# Add the gravity.
+	var wall_sliding = false
+	var wall_direction = 0  # -1 for right wall, 1 for left wall
+
+	# Gravity + Wall Slide
 	if not is_on_floor():
-		velocity.y += gravity * delta
+		if is_on_wall() and velocity.y > 0:
+			velocity.y = min(velocity.y + gravity * delta, wall_slide_speed)
+			wall_sliding = true
 
-	# Handle jump.
-	if Input.is_action_just_pressed("jump") and (is_on_floor() or  is_on_wall()):
-		velocity.y = jump_force
+			# Determine which wall we're on using test_move
+			var left_check = test_move(global_transform, Vector2(-1, 0))
+			var right_check = test_move(global_transform, Vector2(1, 0))
+			if left_check:
+				wall_direction = 1  # wall is on left, jump right
+			elif right_check:
+				wall_direction = -1  # wall is on right, jump left
+		else:
+			velocity.y += gravity * delta
 
+	# Jump
+	if Input.is_action_just_pressed("jump"):
+		if is_on_floor():
+			velocity.y = jump_force
+		elif wall_sliding:
+			velocity.y = jump_force * 0.6
+			velocity.x = wall_jump_pushback * wall_direction  # jump away from wall
+
+	# Jump release shortens jump
 	if Input.is_action_just_released("jump") and velocity.y < 0:
 		velocity.y *= decelerate_on_jump_release
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
+	# Horizontal Movement
 	var direction = Input.get_axis("left", "right")
 	if direction:
-		velocity.x = move_toward(velocity.x, direction * SPEED, SPEED * acceleration) 
+		velocity.x = move_toward(velocity.x, direction * SPEED, SPEED * acceleration)
 		animated_sprite.play("walk")
 		animated_sprite.flip_h = direction < 0
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED * deceleration)
-		animated_sprite.play ("Idle")
-	
-	#Dash Activation
+		animated_sprite.play("Idle")
+
+	# Dash
 	if Input.is_action_pressed("dash") and direction and not is_dashing and dash_timer <= 0:
 		is_dashing = true
 		dash_start_position = position.x
 		dash_direction = direction
-		dash_timer = dash_cooldown 
+		dash_timer = dash_cooldown
 
-	#perform dash
 	if is_dashing:
 		var current_distance = abs(position.x - dash_start_position)
 		if current_distance >= dash_max_distance or is_on_wall():
@@ -62,8 +83,7 @@ func _physics_process(delta):
 		else:
 			velocity.x = dash_direction * dash_speed * dash_curve.sample(current_distance / dash_max_distance)
 			velocity.y = 0
-	
-	#reduce dash timer
+
 	if dash_timer > 0:
 		dash_timer -= delta
 
